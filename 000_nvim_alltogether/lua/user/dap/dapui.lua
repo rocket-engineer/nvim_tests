82da09d9afafb5cde139cb1b879b62ff273d7aae
rocket-engineer <research@man-behind-moon.com>
2022-06-19 14:42:44 +0200

    Replaced deprecated config for DAP UI in overall config

--- 000_nvim_alltogether/lua/user/dap/dapui.lua
+++ 000_nvim_alltogether/lua/user/dap/dapui.lua
@@ -26,22 +26,25 @@ dapui.setup({
     edit   = "e",
     repl   = "r",
   },
-  sidebar = {
-    -- You can change the order of elements in the sidebar
-    elements = {
-      -- Provide as ID strings or tables with "id" and "size" keys
-      {id = "scopes",      size = 0.25}, -- Can be float or integer > 1
-      {id = "breakpoints", size = 0.25},
-      {id = "stacks",      size = 0.25},
-      {id = "watches",     size = 0.25},
+  layouts = {
+    {
+      elements = {
+        'scopes',
+        'breakpoints',
+        'stacks',
+        'watches',
+      },
+      size = 40,
+      position = 'right',
     },
-    position = "right",
-    size     = 40,
-  },
-  tray = {
-    elements = { "repl" },
-    position = "bottom",
-    size     = 10,
+    {
+      elements = {
+        'repl',
+        'console',
+      },
+      size = 10,
+      position = 'bottom',
+    },
   },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
