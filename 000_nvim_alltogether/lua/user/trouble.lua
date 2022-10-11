3fcedfdfb6aadfeaf609b23690f3176f0e5bcb90
vistdn <tobias.dittmann@vector.com>
2022-10-11 11:14:25 +0200

    Added key maps for TroubleToggle

--- 000_nvim_alltogether/lua/user/trouble.lua
+++ 000_nvim_alltogether/lua/user/trouble.lua
@@ -66,3 +66,26 @@ trouble.setup({
 -- Keymaps
 -- =================================================================================================
 
+local wk_ok, wk = pcall(require, "which-key")
+if not wk_ok then
+  return
+end
+
+local opts = {
+  mode    = "n",        -- NORMAL mode
+  prefix  = "<leader>",
+  buffer  = nil,        -- Global mappings. Specify a buffer number for buffer local mappings
+  silent  = true,       -- use `silent` when creating keymaps
+  noremap = true,       -- use `noremap` when creating keymaps
+  nowait  = true,       -- use `nowait` when creating keymaps
+}
+
+local mappings = {
+  l = {
+    name = "LSP",
+    t = {"<cmd>TroubleToggle<cr>", "Toggle Trouble"},
+  }
+}
+
+wk.register(mappings, opts)
+
